8f89606437bdbe6e3cf63437a87978200ed6a5ca
AyrtonCossuol <ayrton.cossuol@live.com>
2020-06-15 09:49:40 -0300

    Commit inicial

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Main from './src/pages/Main';
+
+const App = () => {
+  return(
+    <React.Fragment>  
+      <Main />
+
+    </React.Fragment>
+  );
+};
+
+export default App;
---
+++ src/pages/Main/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+import styles from './styles';
+
+const Main = () => {
+    return (
+        <SafeAreaView style={styles.Container}>
+
+        </SafeAreaView>
+    );
+};
+
+export default Main;
